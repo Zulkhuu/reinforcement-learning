a8c323996d75ef40ceb66e7ccc95150b6b7c475f
Zulkhuu Tuya <t.zulkhuu@gmail.com>
2018-11-29 18:07:49 +0900

    Read parameters from config.py

--- BananaCollector/config.py
+++ BananaCollector/config.py
@@ -1,22 +1,25 @@
 HYPERPARAMS = {
+        #Training parameters
         'env_name':             "Banana_Collector",
         'stop_scores':          13.0,
         'scores_window_size':   100,
-        'train_episodes':       1800,
+        'n_episodes':           2000,
+        'epsilon_start':        1.0,                # starting value of epsilon
+        'epsilon_final':        0.01,               # minimum value of epsilon
+        'epsilon_decay':        0.995,              # factor for decreasing epsilon
 
         # DQN agent parameters
-        'replay_size':          100000,             # replay buffer size
-        'replay_initial':       10000,              # replay buffer initialize
-        'fix_target_updates':   1,                  # fix the target Q for the fix_target_updates
-        'epsilon_start':        1.0,                # starting value of epsilon
-        'epsilon_final':        0.05,               # minimum value of epsilon
-        'epsilon_decay':        0.993,              # factor for decreasing epsilon
+        'seed':                 0,                  # random seed
+        'buffer_size':          10000,              # replay buffer size
+        'batch_size':           64,                 # minibatch size
+        'update_every':         4,                  # network updating every update_interval steps
+        'learning_rate':        5e-4,               # learning rate
+        'tau':                  1e-3,               # for soft update of target parameters
+        'gamma':                0.99,               # discount factor
 
         # Q network parameters
-        'hidden_layers':        [64, 64],           # hidden units and layers of Q-network
-        'update_interval':      4,                  # network updating every update_interval steps
-        'learning_rate':        5e-4,               # learning rate
-        'thau':                 1e-3,               # for soft update of target parameters
-        'gamma':                0.99,               # discount factor
-        'batch_size':           64                  # minibatch size
+        'state_size':           37,                 # state size
+        'action_size':          4,                  # action size
+        'fc1_units':            64,                 # Number of nodes in first hidden layer
+        'fc2_units':            64,                 # Number of nodes in second hidden layer
 }
--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -10,19 +10,12 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 64         # minibatch size
-GAMMA = 0.99            # discount factor
-TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate
-UPDATE_EVERY = 4        # how often to update the network
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
 
-    def __init__(self, state_size, action_size, seed):
+    def __init__(self, param):
         """Initialize an Agent object.
 
         Params
@@ -31,31 +24,38 @@ class Agent():
             action_size (int): dimension of each action
             seed (int): random seed
         """
-        self.state_size = state_size
-        self.action_size = action_size
-        self.seed = random.seed(seed)
+        # Read hyperparameters
+        self.update_every = param['update_every']  # how often to update the network
+        self.buffer_size = param['buffer_size']    # replay buffer size
+        self.batch_size = param['batch_size']      # minibatch size
+        self.gamma = param['gamma']                # discount factor
+        self.tau = param['tau']                    # for soft update of target parameters
+        self.lr = param['learning_rate']           # learning rate
+        state_size = param['state_size']
+        self.action_size = param['action_size']
+        self.seed = random.seed(int(param['seed']))
 
         # Q-Network
-        self.qnetwork_local = QNetwork(state_size, action_size, seed).to(device)
-        self.qnetwork_target = QNetwork(state_size, action_size, seed).to(device)
-        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=LR)
+        self.qnetwork_local = QNetwork(state_size, self.action_size, int(param['seed'])).to(device)
+        self.qnetwork_target = QNetwork(state_size, self.action_size, int(param['seed'])).to(device)
+        self.optimizer = optim.Adam(self.qnetwork_local.parameters(), lr=self.lr)
 
         # Replay memory
-        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
-        # Initialize time step (for updating every UPDATE_EVERY steps)
+        self.memory = ReplayBuffer(self.action_size, self.buffer_size, self.batch_size, int(param['seed']))
+        # Initialize time step (for updating every self.update_every steps)
         self.t_step = 0
 
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
 
-        # Learn every UPDATE_EVERY time steps.
-        self.t_step = (self.t_step + 1) % UPDATE_EVERY
+        # Learn every self.update_every time steps.
+        self.t_step = (self.t_step + 1) % self.update_every
         if self.t_step == 0:
             # If enough samples are available in memory, get random subset and learn
-            if len(self.memory) > BATCH_SIZE:
+            if len(self.memory) > self.batch_size:
                 experiences = self.memory.sample()
-                self.learn(experiences, GAMMA)
+                self.learn(experiences, self.gamma)
 
     def act(self, state, eps=0.):
         """Returns actions for given state as per current policy.
@@ -102,7 +102,7 @@ class Agent():
         self.optimizer.step()
 
         # ------------------- update target network ------------------- #
-        self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)
+        self.soft_update(self.qnetwork_local, self.qnetwork_target, self.tau)
 
     def soft_update(self, local_model, target_model, tau):
         """Soft update model parameters.
