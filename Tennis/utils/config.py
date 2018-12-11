2e1f7378698996e7242a292aaf74f85e3b106328
Zulkhuu Tuya <t.zulkhuu@gmail.com>
2018-12-11 18:26:32 +0900

    feat: Add MADDPG agent

--- Tennis/utils/config.py
+++ Tennis/utils/config.py
@@ -1,8 +1,8 @@
 TRAINPARAMS = {
-    'Reacher':
+    'Tennis':
     {
         #Training parameters
-        'solve_score':          30.0,
+        'solve_score':          0.5,
         'scores_window_size':   100,
         'n_episodes':           2000,
     }
@@ -12,7 +12,8 @@ HYPERPARAMS = {
     'DDPG':
     {
         # Agent parameters
-        'name':                 'DDPG',             # name
+        'name':                 'MADDPG',           # name
+        'number_of_agents'      2,                  # number of agents
         'random_seed':          0,                  # random seed
         'buffer_size':          100000,             # replay buffer size
         'batch_size':           128,                # minibatch size
@@ -23,8 +24,8 @@ HYPERPARAMS = {
         'gamma':                0.99,               # discount factor
 
         # Q network parameters
-        'state_size':           33,                 # state size
-        'action_size':          4,                  # action size
+        'state_size':           24,                 # state size
+        'action_size':          2,                  # action size
         'fc1_units':            256,                # Number of nodes in first hidden layer
         'fc2_units':            128,                # Number of nodes in second hidden layer
     },
