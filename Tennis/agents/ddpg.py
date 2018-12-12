3a5b2b7572465d0d82da5da252bd127458ef9980
Zulkhuu Tuya <t.zulkhuu@gmail.com>
2018-12-12 18:10:15 +0900

    update MADDPG agent
    Change MADDPG agent code to use MARL lab code as baseline.

--- Tennis/agents/ddpg.py
+++ Tennis/agents/ddpg.py
@@ -1,199 +1,81 @@
 import sys
 sys.path.append('../')
 
+from utils.model import Network
+from utils.OUNoise import OUNoise
+from torch.optim import Adam
 import numpy as np
-import random
-import copy
-from collections import namedtuple, deque
-
-from utils.model import Actor, Critic
-
 import torch
-import torch.nn.functional as F
-import torch.optim as optim
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class DDPG():
-    """Interacts with and learns from the environment."""
+class DDPG:
+    def __init__(self, params):
 
-    def __init__(self, param    ):
-        """Initialize an Agent object.
+        in_actor = int(params['state_size'])
+        hidden_in_actor = int(params['fc1_units'])
+        hidden_out_actor = int(params['fc2_units'])
+        out_actor = int(params['action_size'])
+        in_critic = int(params['n_agents'])*(in_actor + out_actor)
+        hidden_in_critic = int(params['fc1_units'])
+        hidden_out_critic = int(params['fc2_units'])
+        lr_actor = params['lr_actor']
+        lr_critic = params['lr_critic']
+        '''
+        print("in_actor:{}".format(in_actor))
+        print("hidden_in_actor:{}".format(hidden_in_actor))
+        print("hidden_out_actor:{}".format(hidden_out_actor))
+        print("out_actor:{}".format(out_actor))
+        print("in_critic:{}".format(in_critic))
+        print("hidden_in_critic:{}".format(hidden_in_critic))
+        print("hidden_out_critic:{}".format(hidden_out_critic))
+        '''
 
-        Params
-        ======
-            state_size (int): dimension of each state
-            action_size (int): dimension of each action
-            random_seed (int): random seed
+        self.actor = Network(in_actor, hidden_in_actor, hidden_out_actor, out_actor, actor=True).to(device)
+        self.critic = Network(in_critic, hidden_in_critic, hidden_out_critic, 1).to(device)
+        self.target_actor = Network(in_actor, hidden_in_actor, hidden_out_actor, out_actor, actor=True).to(device)
+        self.target_critic = Network(in_critic, hidden_in_critic, hidden_out_critic, 1).to(device)
+
+        self.noise = OUNoise(out_actor, params['random_seed'])
+
+        # initialize targets same as original networks
+        self.hard_update(self.target_actor, self.actor)
+        self.hard_update(self.target_critic, self.critic)
+
+        self.actor_optimizer = Adam(self.actor.parameters(), lr=lr_actor)
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=lr_critic, weight_decay=1.e-5)
+
+
+    def act(self, obs, noise=0.5):
+        obs = torch.from_numpy(obs).float().to(device)
+        action = self.actor(obs).cpu() + noise*self.noise.sample()
+        return action.data.numpy()
+
+    def target_act(self, obs, noise=0.5):
+        obs = obs.to(device)
+        action = self.target_actor(obs) + noise*torch.Tensor(self.noise.sample()).cuda()
+        return action
+
+    # https://github.com/ikostrikov/pytorch-ddpg-naf/blob/master/ddpg.py#L11
+    def soft_update(self, target, source, tau):
         """
-        self.buffer_size = int(param['buffer_size'])    # replay buffer size
-        self.batch_size = param['batch_size']           # minibatch size
-        self.gamma = param['gamma']                     # discount factor
-        self.tau = param['tau']                         # for soft update of target parameters
-        self.lr_actor = param['lr_actor']               # learning rate for actor
-        self.lr_critic = param['lr_critic']             # learning rate for critic
-        self.weight_decay = param['weight_decay']       # weight decay
-        fc1_units = int(param['fc1_units'])             # first layer node size (both actor and critic network)
-        fc2_units = int(param['fc2_units'])             # second layer node size (both actor and critic network)
-
-        state_size = param['state_size']
-        action_size = param['action_size']
-        random_seed = param['random_seed']
-
-        self.state_size = state_size
-        self.action_size = action_size
-        self.seed = random.seed(random_seed)
-
-        # Actor Network (w/ Target Network)
-        self.actor_local = Actor(state_size, action_size, random_seed, fc1_units, fc2_units).to(device)
-        self.actor_target = Actor(state_size, action_size, random_seed, fc1_units, fc2_units).to(device)
-        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=self.lr_actor)
-
-        # Critic Network (w/ Target Network)
-        self.critic_local = Critic(state_size, action_size, random_seed, fc1_units, fc2_units).to(device)
-        self.critic_target = Critic(state_size, action_size, random_seed, fc1_units, fc2_units).to(device)
-        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=self.lr_critic, weight_decay=self.weight_decay)
-
-        # Noise process
-        self.noise = OUNoise(action_size, random_seed)
-
-        # Replay memory
-        self.memory = ReplayBuffer(action_size, self.buffer_size, self.batch_size, random_seed)
-
-    def step(self, state, action, reward, next_state, done):
-        """Save experience in replay memory, and use random sample from buffer to learn."""
-        # Save experience / reward
-        self.memory.add(state, action, reward, next_state, done)
-
-        # Learn, if enough samples are available in memory
-        if len(self.memory) > self.batch_size:
-            experiences = self.memory.sample()
-            self.learn(experiences, self.gamma)
-
-    def act(self, state, add_noise=True):
-        """Returns actions for given state as per current policy."""
-        state = torch.from_numpy(state).float().to(device)
-        self.actor_local.eval()
-        with torch.no_grad():
-            action = self.actor_local(state).cpu().data.numpy()
-        self.actor_local.train()
-        if add_noise:
-            action += self.noise.sample()
-        return np.clip(action, -1, 1)
-
-    def reset(self):
-        self.noise.reset()
-
-    def learn(self, experiences, gamma):
-        """Update policy and value parameters using given batch of experience tuples.
-        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
-        where:
-            actor_target(state) -> action
-            critic_target(state, action) -> Q-value
-
-        Params
-        ======
-            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
-            self.gamma (float): discount factor
+        Perform DDPG soft update (move target params toward source based on weight
+        factor tau)
+        Inputs:
+            target (torch.nn.Module): Net to copy parameters to
+            source (torch.nn.Module): Net whose parameters to copy
+            tau (float, 0 < x < 1): Weight factor for update
         """
-        states, actions, rewards, next_states, dones = experiences
+        for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(target_param.data * (1.0 - tau) + param.data * tau)
 
-        # ---------------------------- update critic ---------------------------- #
-        # Get predicted next-state actions and Q values from target models
-        actions_next = self.actor_target(next_states)
-        Q_targets_next = self.critic_target(next_states, actions_next)
-        # Compute Q targets for current states (y_i)
-        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
-        # Compute critic loss
-        Q_expected = self.critic_local(states, actions)
-        critic_loss = F.mse_loss(Q_expected, Q_targets)
-        # Minimize the loss
-        self.critic_optimizer.zero_grad()
-        critic_loss.backward()
-        self.critic_optimizer.step()
-
-        # ---------------------------- update actor ---------------------------- #
-        # Compute actor loss
-        actions_pred = self.actor_local(states)
-        actor_loss = -self.critic_local(states, actions_pred).mean()
-        # Minimize the loss
-        self.actor_optimizer.zero_grad()
-        actor_loss.backward()
-        self.actor_optimizer.step()
-
-        # ----------------------- update target networks ----------------------- #
-        self.soft_update(self.critic_local, self.critic_target, self.tau)
-        self.soft_update(self.actor_local, self.actor_target, self.tau)
-
-    def soft_update(self, local_model, target_model, tau):
-        """Soft update model parameters.
-        θ_target = τ*θ_local + (1 - τ)*θ_target
-
-        Params
-        ======
-            local_model: PyTorch model (weights will be copied from)
-            target_model: PyTorch model (weights will be copied to)
-            self.tau (float): interpolation parameter
+    # https://github.com/ikostrikov/pytorch-ddpg-naf/blob/master/ddpg.py#L15
+    def hard_update(self, target, source):
         """
-        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
-
-class OUNoise:
-    """Ornstein-Uhlenbeck process."""
-
-    def __init__(self, size, seed, mu=0., theta=0.15, sigma=0.2):
-        """Initialize parameters and noise process."""
-        self.mu = mu * np.ones(size)
-        self.size = size
-        self.theta = theta
-        self.sigma = sigma
-        self.seed = random.seed(seed)
-        self.reset()
-
-    def reset(self):
-        """Reset the internal state (= noise) to mean (mu)."""
-        self.state = copy.copy(self.mu)
-
-    def sample(self):
-        """Update internal state and return it as a noise sample."""
-        x = self.state
-        dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
-        self.state = x + dx
-        return self.state
-
-class ReplayBuffer:
-    """Fixed-size buffer to store experience tuples."""
-
-    def __init__(self, action_size, buffer_size, batch_size, seed):
-        """Initialize a ReplayBuffer object.
-        Params
-        ======
-            buffer_size (int): maximum size of buffer
-            batch_size (int): size of each training batch
+        Copy network parameters from source to target
+        Inputs:
+            target (torch.nn.Module): Net to copy parameters to
+            source (torch.nn.Module): Net whose parameters to copy
         """
-        self.action_size = action_size
-        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
-        self.batch_size = batch_size
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
-        self.seed = random.seed(seed)
-
-    def add(self, state, action, reward, next_state, done):
-        """Add a new experience to memory."""
-        e = self.experience(state, action, reward, next_state, done)
-        self.memory.append(e)
-
-    def sample(self):
-        """Randomly sample a batch of experiences from memory."""
-        experiences = random.sample(self.memory, k=self.batch_size)
-
-        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
-        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
-        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
-        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
-
-        return (states, actions, rewards, next_states, dones)
-
-    def __len__(self):
-        """Return the current size of internal memory."""
-        return len(self.memory)
+        for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(param.data)
--- Tennis/utils/config.py
+++ Tennis/utils/config.py
@@ -1,32 +1,31 @@
-TRAINPARAMS = {
+HYPERPARAMS = {
     'Tennis':
     {
-        #Training parameters
-        'solve_score':          0.5,
-        'scores_window_size':   100,
-        'n_episodes':           2000,
-    }
-}
+        # Environment parameters
+        'env_name':             'Tennis',           # environment name
+        'solve_score':          0.5,                # score to solve environment
+        'scores_window_size':   100,                # moving average window size
+        'n_episodes':           2000,               # maximum number of episodes
 
-HYPERPARAMS = {
-    'MADDPG':
-    {
-        # Agent parameters
-        'name':                 'MADDPG',           # name
-        'n_agents':             2,                  # number of agents
-        'random_seed':          0,                  # random seed
-        'buffer_size':          100000,             # replay buffer size
-        'batch_size':           128,                # minibatch size
-        'weight_decay':         0.0001,             # weight decay
-        'lr_actor':             1e-4,               # learning rate for actor
-        'lr_critic':            1e-4,               # learning rate for critic
-        'tau':                  1e-3,               # for soft update of target parameters
-        'gamma':                0.99,               # discount factor
+        'agent':
+        {
+            # Agent parameters
+            'agent_name':           'MADDPG',           # name
+            'n_agents':             2,                  # number of agents
+            'random_seed':          51,                  # random seed
+            'buffer_size':          1000000,             # replay buffer size
+            'batch_size':           128,                # minibatch size
+            'weight_decay':         0.0001,             # weight decay
+            'lr_actor':             1e-3,               # learning rate for actor
+            'lr_critic':            1e-3,               # learning rate for critic
+            'tau':                  1e-5,               # for soft update of target parameters
+            'gamma':                0.999,              # discount factor
 
-        # Q network parameters
-        'state_size':           24,                 # state size
-        'action_size':          2,                  # action size
-        'fc1_units':            256,                # Number of nodes in first hidden layer
-        'fc2_units':            128,                # Number of nodes in second hidden layer
+            # Q network parameters
+            'state_size':           24,                 # state size
+            'action_size':          2,                  # action size
+            'fc1_units':            128,                # Number of nodes in first hidden layer
+            'fc2_units':            150,                # Number of nodes in second hidden layer
+        }
     },
 }
